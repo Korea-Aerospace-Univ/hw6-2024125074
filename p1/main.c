ab99b3fc028894706533666d80ad8b0300101e71
2024125074 <gayoun0805@naver.com>
2026-06-01 15:50:52 +0900

    Revise character counting and input method in main.c
    Refactor character counting logic and improve input handling.

--- p1/main.c
+++ p1/main.c
@@ -2,27 +2,28 @@
 
 int main(void)
 {   
-    char arr[11]={};
-    int count, maxWcount=0;
-    char *p, *q;
-    char W,maxW;
+    char arr[10]={}; //문자열 배열 선언과 동시에 초기화
+    int count=0, maxWcount=0; //count는 연속되는 문자의 수,maxWcount는 연속된 문자의 최댓값
+    char *p, *q; //이중반복문을 위한 두가지 포인터 선언
+    char W,maxW; //W는 입력받을 문자,maxW는 가장 많이 나타난 문자
     
-    scanf("%10s",arr);
+    for (p=arr; p<arr+10;p++) //포인터 사용해서 문자 10개 입력받을 때까지 반복
+    scanf("%c",p); // 문자 입력받기
         
-    for (p=arr; p<arr+10; p++) {
-        count=0;
-        for (q=arr; q<arr+10; q++) {
-            if (*p==*q) {
-                count++;
+    for (p=arr; p<arr+10; p++) { //문자 10개까지 반복
+        count =0; //문자가 다르면 세는 횟수 초기화 
+        for (q=arr; q<arr+10; q++) { //이중반복문 문자 10개까지 반복
+            if (*p==*q) { //문자가 같으면
+                count++; //연속되는 문자의 수 증가
             }
         }
-        if (count>maxWcount) {
-            maxWcount=count;
-            maxW=*p;
+        if (count>maxWcount) { //만약 연속되는 문자의 수가 최댓값보다 크면
+            maxWcount=count; //연속되는 문자의 최댓값 갱신
+            maxW=*p; //가장 많이 반복되는 문자에 현재 포인터 값 입력,순서대로 반복되는 반복문 특성에 의해 연속되는 문자의 수가 같은 경우 알아서 먼저 입력받은 문자가 출력
         }
     }
     
-    printf("%c %d",maxW,maxWcount);
+    printf("%c %d",maxW,maxWcount); //가장 많이 반복되는 문자와 연속되는 문자의 최댓값 출력
    
 
     return 0;
